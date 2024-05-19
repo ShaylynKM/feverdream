f5da9f5fa547309e3331e1664e8ff6252a43d04d
Shaylyn Melin <shaylynmelin@gmail.com>
2024-05-18 21:49:43 -0600

    Template
    Made template scene for the game's interface and a prefab + started a scriptable object with stuff that'll be in each scene

---
+++ Assets/_FEVERDREAM/2_Scripts/ScriptableObjects/UIScriptableObject.cs
@@ -0,0 +1,24 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScriptableObject : ScriptableObject
+{
+    public Sprite Background;
+    public Sprite DialogueBox;
+
+    public Button UpArrow;
+    public Button DownArrow;
+    public Button LeftArrow;
+    public Button RightArrow;
+
+    public TextMeshProUGUI DialogueText;
+
+    public Button MainMenuButton;
+    public Button SettingsButton;
+    public Button SaveLoadButton;
+    public Button QuitButton;
+
+    // Will need dialogue option buttons. Currently deciding how I plan to do this. Want to potentially do what you can do in Twine where certain words are clickable.
+}
