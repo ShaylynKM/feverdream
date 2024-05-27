fc8425b1a8b166ec13a1a175a71e4a1ceb4facf0
Shaylyn Melin <shaylynmelin@gmail.com>
2024-05-26 23:27:40 -0600

    Added a quick script for my prototype
    (Demo, mostly)

--- Assets/_FEVERDREAM/2_Scripts/ScriptableObjects/UIScriptableObject.cs
+++ Assets/_FEVERDREAM/2_Scripts/ScriptableObjects/UIScriptableObject.cs
@@ -1,27 +1,27 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
+//using Microsoft.Unity.VisualStudio.Editor;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-[CreateAssetMenu]
-public class UIScriptableObject : ScriptableObject
-{
-    public Sprite Background;
-    public Sprite DialogueBox;
+//[CreateAssetMenu]
+//public class UIScriptableObject : ScriptableObject
+//{
+//    public Sprite Background;
+//    public Sprite DialogueBox;
 
-    public Button UpArrow;
-    public Button DownArrow;
-    public Button LeftArrow;
-    public Button RightArrow;
+//    public Button UpArrow;
+//    public Button DownArrow;
+//    public Button LeftArrow;
+//    public Button RightArrow;
 
-    public TextMeshProUGUI DialogueText;
+//    public TextMeshProUGUI DialogueText;
 
-    public Button MainMenuButton;
-    public Button SettingsButton;
-    public Button SaveLoadButton;
-    public Button QuitButton;
+//    public Button MainMenuButton;
+//    public Button SettingsButton;
+//    public Button SaveLoadButton;
+//    public Button QuitButton;
 
-    public Button[] OptionButtons;
+//    public Button[] OptionButtons;
 
-    // Will need dialogue option buttons. Currently deciding how I plan to do this. Want to potentially do what you can do in Twine where certain words are clickable.
-}
+//    // Will need dialogue option buttons. Currently deciding how I plan to do this. Want to potentially do what you can do in Twine where certain words are clickable.
+//}
