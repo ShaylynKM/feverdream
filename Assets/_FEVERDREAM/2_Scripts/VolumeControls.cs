a9cb2445098a4dd8a88385e866a16e40a4c28e73
Shaylyn Melin <shaylynmelin@gmail.com>
2024-06-12 00:13:07 -0600

    Volume controls working with 1 bug
    Need help (thumbs up)

--- Assets/_FEVERDREAM/2_Scripts/VolumeControls.cs
+++ Assets/_FEVERDREAM/2_Scripts/VolumeControls.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VolumeControls : MonoBehaviour
 {
+    // CURRENT ISSUE: This code is working flawlessly, except the AudioListener shows Inactive in the Mixer between scenes.
+
     public static VolumeControls Instance;
 
     [SerializeField]
@@ -18,38 +21,73 @@ public class VolumeControls : MonoBehaviour
     private const float _minValue = -20f;
     private const float _maxValue = 0f;
 
+    [SerializeField]
     private float _currentSavedVolume;
-
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(this.gameObject);
+            DontDestroyOnLoad(gameObject);
         }
         else if (Instance != this)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
+            return;
         }
     }
 
     private void Start()
     {
-        _currentSavedVolume = PlayerPrefs.GetFloat("Volume", _maxValue);
+        SceneManager.activeSceneChanged += OnActiveSceneChanged; // Subscribes our scene changing method to the callback event
 
-        float decibels = Mathf.Lerp(_minValue, _maxValue, _currentSavedVolume); // Convert the saved volume from its linear value
+        _currentSavedVolume = PlayerPrefs.GetFloat("Volume", _maxValue); // Assigns the volume as the saved volume (unless there is no saved volume, then the default, maximum level is used
 
-        _audioMixer.SetFloat("MasterVolume", decibels);
+        OnSliderFound();
+    }
 
-        _volumeSlider.value = _currentSavedVolume;
-        _volumeSlider.onValueChanged.AddListener(SetVolume); // Adds listener to the slider
+    void OnActiveSceneChanged(Scene previous, Scene next)
+    {
+        OnSliderFound();
+
+        SetVolume(_currentSavedVolume);
+        Debug.Log("Volume set");
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= OnActiveSceneChanged; // Unsubscribes from the callback method when this object is destroyed
     }
 
     public void SetVolume(float volume)
     {
+        _currentSavedVolume = volume;
+        PlayerPrefs.SetFloat("Volume", _currentSavedVolume);
+
         _audioMixer.SetFloat("MasterVolume", volume);
 
         PlayerPrefs.SetFloat("Volume", volume);
+    }
+
+    private void OnSliderFound()
+    {
+        _volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>(); // Will change from Find
+
+        if(_volumeSlider != null)
+        {
+            _volumeSlider.onValueChanged.AddListener(SetVolume);
+
+            _volumeSlider.value = _currentSavedVolume;
+
+            Debug.Log("assigned the listener");
+
+
+        }
+        else
+        {
+            Debug.Log("Slider not found");
+        }
+
     }
 }
