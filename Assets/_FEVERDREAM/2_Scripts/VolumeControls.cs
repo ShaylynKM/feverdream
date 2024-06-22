0f6b925086437063f801b45a7ff246d80fc27ec3
Shaylyn Melin <shaylynmelin@gmail.com>
2024-06-22 15:46:13 -0600

    Voice SFX toggle working

--- Assets/_FEVERDREAM/2_Scripts/VolumeControls.cs
+++ Assets/_FEVERDREAM/2_Scripts/VolumeControls.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class VolumeControls : MonoBehaviour
 {
@@ -13,13 +14,21 @@ public class VolumeControls : MonoBehaviour
     private AudioMixer _audioMixer;
 
     [SerializeField]
-    private Slider _volumeSlider;
+    private Slider _volumeSlider; // For the overall volume
 
-    // Audio volume in db
-    private const float _maxValue = 0f;
+    [SerializeField]
+    private Toggle _voiceToggle; // For muting the voiced effects
+
+    private const float _maxValue = 0f; // Audio volume in db
 
     [SerializeField]
     private float _currentSavedVolume;
+
+    [SerializeField]
+    private bool _voiceIsAudible = true;
+
+    [SerializeField]
+    private AudioSource _typeSFX;
 
     private void Awake()
     {
@@ -41,15 +50,22 @@ public class VolumeControls : MonoBehaviour
 
         _currentSavedVolume = PlayerPrefs.GetFloat("Volume", _maxValue); // Assigns the volume as the saved volume (unless there is no saved volume, then the default, maximum level is used
 
+        _voiceIsAudible = PlayerPrefs.GetInt("VoiceMute", 1) == 1;
+
         OnSliderFound();
+        OnToggleFound();
+        AssignTypeSFX();
+        SetVolume(_currentSavedVolume);
+        SFXMuteState();
     }
 
     void OnActiveSceneChanged(Scene previous, Scene next)
     {
         OnSliderFound();
-
+        OnToggleFound();
+        AssignTypeSFX();
         SetVolume(_currentSavedVolume);
-        Debug.Log("Volume set");
+        SFXMuteState();
     }
 
     private void OnDestroy()
@@ -57,9 +73,11 @@ public class VolumeControls : MonoBehaviour
         SceneManager.activeSceneChanged -= OnActiveSceneChanged; // Unsubscribes from the callback method when this object is destroyed
     }
 
+    // Sets the volume for all audio
     public void SetVolume(float volume)
     {
         _currentSavedVolume = volume;
+
         PlayerPrefs.SetFloat("Volume", _currentSavedVolume);
 
         _audioMixer.SetFloat("MasterVolume", volume);
@@ -67,12 +85,23 @@ public class VolumeControls : MonoBehaviour
         PlayerPrefs.SetFloat("Volume", volume);
     }
 
+    // Finds and assigns a new volume slider for when the scene changes
     private void OnSliderFound()
     {
-        // Finds and assigns a new volume slider for when the scene changes
-        _volumeSlider = FindObjectOfType<Slider>(true);
+        Slider[] sliders = FindObjectsOfType<Slider>(true);
 
-        if(_volumeSlider != null && _volumeSlider.name == "VolumeSlider")
+        // Makes sure we find the correct slider before proceeding
+        foreach(Slider slider in sliders)
+        {
+            if(slider.name == "VolumeSlider")
+            {
+                _volumeSlider = slider;
+
+                break;
+            }
+        }
+
+        if(_volumeSlider != null)
         {
             _volumeSlider.onValueChanged.AddListener(SetVolume);
             _volumeSlider.value = _currentSavedVolume;
@@ -85,4 +114,62 @@ public class VolumeControls : MonoBehaviour
 
     }
 
+    // Assigns the muted/unmuted state of the mute toggle in relation to the voice SFX
+    private void SFXMuteState()
+    {
+        if (_typeSFX != null)
+        {
+            _typeSFX.mute = !_voiceIsAudible;
+        }
+    }
+
+    public void OnVoiceMute(bool isMuted)
+    {
+        _voiceIsAudible = !isMuted;
+        PlayerPrefs.SetInt("VoiceMute", _voiceIsAudible ? 1 : 0); // Saves the bool in PlayerPrefs as an Int, since PlayerPrefs doesn't natively support boolean values
+        SFXMuteState();
+    }
+
+    // Finds and assigns a new voice effect toggle for when the scene changes
+    private void OnToggleFound()
+    {
+        Toggle[] toggles = FindObjectsOfType<Toggle>(true);
+
+        // Makes sure we find the correct toggle before proceeding
+        foreach (Toggle toggle in toggles)
+        {
+            if (toggle.name == "VoiceToggle")
+            {
+                _voiceToggle = toggle;
+
+                break;
+            }
+        }
+
+        if (_voiceToggle != null)
+        {
+            // Assigns the listener for the value changed
+            _voiceToggle.onValueChanged.AddListener(delegate
+            {
+                OnVoiceMute(!_voiceToggle.isOn);
+            });
+
+            _voiceToggle.isOn = _voiceIsAudible; // Sets the toggle in the scene to on or off based on the current mute state
+        }
+        else
+        {
+            Debug.Log("Toggle not found");
+        }
+    }
+
+    // Finds and assigns the typing sounds
+    private void AssignTypeSFX()
+    {
+        GameObject typeSFXObject = GameObject.FindWithTag("TypingSFX"); // Finds the object with the audio source
+
+        if (typeSFXObject != null)
+        {
+            _typeSFX = typeSFXObject.GetComponent<AudioSource>();           
+        }
+    }
 }
