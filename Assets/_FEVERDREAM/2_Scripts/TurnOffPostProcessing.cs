727d153c4f067d414085e2808db9f5b579b44ab8
Shaylyn Melin <shaylynmelin@gmail.com>
2024-06-23 01:38:23 -0600

    Post process toggle working
    the pp

---
+++ Assets/_FEVERDREAM/2_Scripts/TurnOffPostProcessing.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TurnOffPostProcessing : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle _ppToggle; // Nobody say a damn thing about this variable name
+
+    [SerializeField]
+    private bool _ppEnabled = true; // Or this one
+
+    private PostProcessVolume _ppVolume; // Or this
+
+    private void Awake()
+    {
+        _ppEnabled = PlayerPrefs.GetInt("PostProcessingOnOff", 1) == 1; // Applies the post processing effects immediately
+
+        // This script goes on the main camera. Get the volume from it
+        _ppVolume = GetComponent<PostProcessVolume>();
+        Debug.Log("got the pp");
+
+        TurnPPOnAndOff();
+    }
+
+    private void Start()
+    {
+        OnToggleFound();
+    }
+
+    private void OnToggleFound()
+    {
+        Toggle[] toggles = FindObjectsOfType<Toggle>(true);
+
+        // Makes sure we find the correct toggle before proceeding
+        foreach (Toggle toggle in toggles)
+        {
+            if (toggle?.name == "PPToggle")
+            {
+                _ppToggle = toggle;
+                break;
+            }
+        }
+
+        if (_ppToggle != null)
+        {
+            // Assigns the listener for the value changed
+            _ppToggle.onValueChanged.AddListener(delegate
+            {
+                OnPPToggle(_ppToggle.isOn);
+            });
+
+            _ppToggle.isOn = _ppEnabled; // Sets the toggle in the scene to on or off based on the current state
+        }
+        else
+        {
+            Debug.Log("Post processing toggle not found");
+        }
+    }
+
+    private void OnPPToggle(bool isEnabled)
+    {
+        _ppEnabled = isEnabled;
+
+        PlayerPrefs.SetInt("PostProcessingOnOff", _ppEnabled ? 1 : 0); // Saves the bool in PlayerPrefs as an Int, since PlayerPrefs doesn't natively support boolean values
+
+        TurnPPOnAndOff();
+
+    }
+
+    private void TurnPPOnAndOff()
+    {
+        // Turns the post process volume on and off based on the bool value
+
+        if (_ppEnabled == true)
+        {
+            _ppVolume.enabled = true;
+        }
+        else
+        {
+            _ppVolume.enabled = false;
+        }
+    }
+}
